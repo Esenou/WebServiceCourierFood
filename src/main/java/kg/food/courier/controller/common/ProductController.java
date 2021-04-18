afe92ede23c4252ee68fc1c5e94537edcc67fa43
Esenou <36571539+Esenou@users.noreply.github.com>
2021-04-18 17:40:50 +0600

    add service product

--- src/main/java/kg/food/courier/controller/common/ProductController.java
+++ src/main/java/kg/food/courier/controller/common/ProductController.java
@@ -3,10 +3,12 @@ package kg.food.courier.controller.common;
 import kg.food.courier.controller.base.BaseController;
 import kg.food.courier.entity.Product;
 import kg.food.courier.service.ProductService;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ProductController extends BaseController<Product, ProductService> {
-    private final ProductService productService;
 
+    private final ProductService productService;
     public ProductController(ProductService productService) {
         super(productService);
         this.productService = productService;
